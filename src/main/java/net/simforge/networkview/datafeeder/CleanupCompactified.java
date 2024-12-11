2b0fa0b4af2bed5f046af1e49376cf9a0b4cba22
alex853 <alexxxey.kornev@gmail.com>
2024-12-11 14:23:44 +0200

    CleanupCompactified task added

---
+++ src/main/java/net/simforge/networkview/datafeeder/CleanupCompactified.java
@@ -0,0 +1,93 @@
+package net.simforge.networkview.datafeeder;
+
+import net.simforge.commons.legacy.BM;
+import net.simforge.commons.misc.JavaTime;
+import net.simforge.commons.runtime.BaseTask;
+import net.simforge.commons.runtime.RunningMarker;
+import net.simforge.networkview.core.Network;
+import net.simforge.networkview.core.report.ReportUtils;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class CleanupCompactified extends BaseTask {
+    private static final String ARG_NETWORK = "network";
+    private static final String ARG_STORAGE = "storage";
+    private static final String ARG_KEEP_DAYS = "keep-days";
+
+    private final Network network;
+    private final int keepDays;
+    private final String storageRoot;
+
+    private final CompactifiedStorage storage;
+
+    @SuppressWarnings("unused")
+    public CleanupCompactified(final Properties properties) {
+        this(Network.valueOf(properties.getProperty(ARG_NETWORK)), properties);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public CleanupCompactified(final Network network, final Properties properties) {
+        super("CleanCompact-" + network);
+        this.network = network;
+        this.keepDays = Math.max(Integer.parseInt(properties.getProperty(ARG_KEEP_DAYS)), 1);
+        this.storageRoot = properties.getProperty(ARG_STORAGE, "./data");
+        this.storage = CompactifiedStorage.getStorage(storageRoot, network);
+    }
+
+    @Override
+    protected void startup() {
+        super.startup();
+
+        BM.setLoggingPeriod(TimeUnit.HOURS.toMillis(1));
+
+        RunningMarker.lock(getTaskName());
+
+        logger.info("Network        : " + network);
+        logger.info("Storage root   : " + storageRoot);
+        logger.info("Keep days      : " + keepDays);
+    }
+
+    @Override
+    protected void process() {
+        BM.start("CleanupCompactified.process");
+        try {
+            final LocalDateTime thresholdDt = JavaTime.nowUtc().minusDays(keepDays);
+
+            final String report = storage.getFirstReport();
+            if (report == null) {
+                logger.debug("No reports found");
+                return; // standard sleep time
+            }
+
+            final LocalDateTime reportDt = ReportUtils.fromTimestampJava(report);
+            if (reportDt.isAfter(thresholdDt)) {
+                logger.debug(ReportUtils.log(report) + " -     Threshold is {}, nothing to cleanup", JavaTime.yMdHms.format(thresholdDt));
+                return; // standard sleep time
+            }
+
+            removeReport(report);
+
+            logger.info(ReportUtils.log(report) + " - Cleanup complete");
+
+            setNextSleepTime(1000); // short sleep time
+        } catch (IOException e) {
+            logger.error("error on processing", e);
+        } finally {
+            BM.stop();
+        }
+    }
+
+    private void removeReport(final String report) {
+        BM.start("CleanupCompactified.removeReport");
+        try {
+            storage.removeReport(report);
+        } catch (IOException e) {
+            logger.error("error on removal", e);
+        } finally {
+            BM.stop();
+        }
+    }
+}
--- src/main/java/net/simforge/networkview/datafeeder/CompactifiedStorage.java
+++ src/main/java/net/simforge/networkview/datafeeder/CompactifiedStorage.java
@@ -1,13 +1,18 @@
 package net.simforge.networkview.datafeeder;
 
+import net.simforge.commons.legacy.BM;
 import net.simforge.networkview.core.CompactifiedPosition;
 import net.simforge.networkview.core.Network;
 import net.simforge.networkview.core.Position;
+import net.simforge.networkview.core.report.ReportUtils;
 import net.simforge.networkview.core.report.persistence.ReportPilotPosition;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 // todo ak separate file-writer-reader for specific version and tests on it
@@ -17,11 +22,11 @@ public class CompactifiedStorage {
 
     private final Path root;
 
-    private CompactifiedStorage(String rootPath, Network network) {
+    private CompactifiedStorage(final String rootPath, final Network network) {
         root = Paths.get(rootPath, network.name(), "/compactified");
     }
 
-    public static CompactifiedStorage getStorage(String storageRoot, Network network) {
+    public static CompactifiedStorage getStorage(final String storageRoot, final Network network) {
         return new CompactifiedStorage(storageRoot, network);
     }
 
@@ -48,9 +53,94 @@ public class CompactifiedStorage {
             if (!Files.exists(root)) {
                 Files.createDirectory(root);
             }
-            Files.write(root.resolve(report), baos.toByteArray(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            Files.write(root.resolve(report),
+                    baos.toByteArray(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING,
+                    StandardOpenOption.WRITE);
         } catch (final IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String getFirstReport() throws IOException {
+        final List<String> allReports = listAllReportFiles();
+        if (allReports.size() == 0) {
+            return null;
+        }
+        return allReports.get(0);
+    }
+
+    public String getNextReport(String previousReport) throws IOException {
+        final List<String> allReports = listAllReportFiles();
+        if (allReports.size() == 0) {
+            return null;
+        }
+        final int index = allReports.indexOf(previousReport);
+        if (index == -1) {
+            return null;
+        }
+        if (index == allReports.size() - 1) {
+            return null;
+        }
+        return allReports.get(index + 1);
+    }
+
+    public String getLastReport() throws IOException {
+        final List<String> allReports = listAllReportFiles();
+        if (allReports.size() == 0) {
+            return null;
+        }
+        return allReports.get(allReports.size() - 1);
+    }
+
+    private List<String> listAllReportFiles() throws IOException {
+        BM.start("CompactifiedStorage.listAllReportFiles");
+        try {
+            final List<String> reports = new ArrayList<>();
+
+            Files.walkFileTree(root, new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs) {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) {
+                    if (!attrs.isRegularFile()) {
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    final String filename = file.getFileName().toString();
+                    if (!ReportUtils.isTimestamp(filename)) {
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    reports.add(filename);
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(final Path file, final IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(final Path dir, final IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            Collections.sort(reports);
+
+            return reports;
+        } finally {
+            BM.stop();
+        }
+    }
+
+    public void removeReport(final String report) throws IOException {
+        Files.deleteIfExists(root.resolve(report));
     }
 }
